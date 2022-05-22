d03265f8cf26bea96115aabd9314de0c97e8f9c2
Tamilarasu AlmaLinux <debian@ubuntu.com>
2022-05-22 22:17:38 +0530

    Unattended oh-my-zsh install

---
+++ initialsetup.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+#Check if script is executed with root permissions
+
+# if [[ "${UID}" -ne 0 ]]
+# then
+#     echo "Please execute this script with sudo / root permissions"
+#     exit 1
+# fi
+
+# Create a file for logs
+touch logfile.txt
+
+# Distro information
+
+DISTRO=$(. /etc/os-release && echo "$ID")
+
+#Install zsh
+
+## Install zsh in debian
+debian_install()
+{
+    echo "Updates going on !!!"
+    sudo apt-get update &>>logfile.txt
+    echo "Installing zsh"
+    sudo apt install zsh -y &>>logfile.txt
+
+    if [[ "${?}" -ne 0 ]]
+    then
+        echo "Script could not proceed hereafter !!!"
+        echo "Please check logfile.txt in current directory for logs"
+        exit 1
+    fi
+}
+
+centos_install()
+{
+    echo "Updates going on !!!"
+    sudo yum -y update &>>logfile.txt
+    echo "Installing zsh"
+    sudo yum install -y zsh &>>logfile.txt
+
+    if [[ "${?}" -ne 0 ]]
+    then
+        echo "Script could not proceed hereafter !!!"
+        echo "Please check logfile.txt in current directory for logs"
+        exit 1
+    fi
+}
+
+case $DISTRO in
+    ubuntu|debian)
+        debian_install
+        ;;
+    centos|rhel|almalinux|rockylinux)
+        centos_install
+        ;;
+esac
+
+# Change defalt shell to ZSH
+
+ZSH_PRESENT=$(cat /etc/shells | grep zsh)
+
+if [[ "${?}" -ne 0 ]]
+then
+    echo "ZSH is not present !!!"
+    exit 1
+fi
+
+sudo chsh -s $(which zsh) $(whoami)
+
+if [[ "${?}" -ne 0 ]]
+then
+    echo "Shell not changed due to an error !!!"
+    exit 1
+fi
+
+# Oh-my-zsh script
+LINK="https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
+
+# Check if wget or curl exists
+if  ! command -v wget &>/dev/null
+then
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --skip-chsh"
+else
+    sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -) --skip-chsh"
+fi
+
+if [[ "${?}" -ne 0 ]]
+then
+    echo "Error in installing oh-my-zsh"
+    exit 1
+fi
